ffc8498b4d02e0a45592d70832e5e699f0fb0629
pavitpim40_mbp <pavitpim@gmail.com>
2024-05-03 13:17:01 +0700

    feat:get,delete,post

---
+++ src/index.js
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import axios from 'axios';
+import './index.css';
+/*
+Server-State (Backend)
+TODO : {userId?:number,id:number,title:string,complete:boolean}
+TODO_LISTS : Array<TODO>
+*/
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  // FN BODY : Logic
+
+  // API-1 : getAllTodo
+  async function getAllTodo() {
+    try {
+      const response = await axios.get('http://jsonplaceholder.typicode.com/todos');
+      // Backend State => Frontend State
+      setTodos(response.data.slice(0, 11));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  // API-2 : createTodo
+  // ### USER -> React State
+  const handleChangeTodo = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  // ### React State => Server State
+  const createTodo = async () => {
+    const data = { title: newTodo, complete: false };
+    try {
+      let response = await axios.post('https://jsonplaceholder.typicode.com/todos', data);
+      // console.log(response.status, response.data);
+      setTodos([response.data, ...todos]);
+      setNewTodo('');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // API-3 : Delete Todo
+  const deleteTodo = async (todoId) => {
+    console.log('try to delete todoId :', todoId);
+    if (!todoId) return;
+    try {
+      // React State => Server
+      await axios.delete(`https://jsonplaceholder.typicode.com/todos/${todoId}`);
+
+      // Server => React [OK]
+      // ไม่ใช่ status 400,500 แน่นนอน
+      console.log('delete success');
+
+      // React State => UI
+      // #1
+      // const newTodoList = [...todos];
+      // const foundedIndex = newTodoList.findIndex((todo) => todo.id === todoId);
+      // if (foundedIndex !== -1) {
+      //   newTodoList.splice(foundedIndex, 1);
+      //   setTodos(newTodoList);
+      // }
+      // #2
+      setTodos((prev) => prev.filter((todo) => todo.id !== todoId));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // FN RETURN : UI
+  return (
+    <div className='app'>
+      <div className='todo'>
+        <header className='todo__add'>
+          <h1>
+            TodoList <button onClick={getAllTodo}>FETCH</button>
+          </h1>
+          <input value={newTodo} onChange={handleChangeTodo} />
+          <button onClick={createTodo}>add</button>
+        </header>
+        <ul className='todo__list'>
+          {todos.map((todo) => (
+            <li key={todo.id} className='todo__item'>
+              <p className={todo.complete ? 'done' : ''}>{todo.title}</p>
+              <button>edit</button>
+              {/* <button onClick={deleteTodo}>x</button> */}
+              <button onClick={(event) => deleteTodo(todo.id)}>x</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
